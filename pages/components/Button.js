75f48e33fc292927d40bb026e8e27b8c5463c6dd
Atolagbe Elisha <atolagbeelisha001>
2021-11-27 10:28:12 +0100

    bug link fix

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -5,8 +5,7 @@ const Button = (props) => {
   return (
     <Link
       href={{
-        pathname: "/about",
-        query: { name: "test" },
+        pathname: props.mylink,
       }}
     >
       <div className={`border-1 h-14 rounded-2xl border-${props.color}-400 px-5 text-${props.color}-400 flex items-center justify-center cursor-pointer w-full font-semibold`}>{props.text}</div>
