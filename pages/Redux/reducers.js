60608573aece3e72250dd7e472af8f7561124675
Atolagbe Elisha <atolagbeelisha001>
2021-11-27 06:19:01 +0100

    second safe commit

--- pages/Redux/reducers.js
+++
@@ -1,7 +0,0 @@
-// import { combineReducers } from "redux";
-
-// const messageReducer = (state = [], action) => {
-//   return { ...state };
-// };
-
-// export default reducers = combineReducers({});
