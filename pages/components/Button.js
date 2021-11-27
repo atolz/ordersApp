8e97e34af649970bc2b20d975b538bdb660046ab
Atolagbe Elisha <atolagbeelisha001>
2021-11-27 14:24:56 +0100

    funny bug fix?

--- pages/components/Button.js
+++ pages/components/Button.js
@@ -2,13 +2,21 @@ import React from "react";
 import Link from "next/link";
 
 const Button = (props) => {
-  return (
+  return props.color == "blue" ? (
     <Link
       href={{
         pathname: props.mylink,
       }}
     >
-      <div className={`border-1 h-14 rounded-2xl border-${props.color}-400 px-5 text-${props.color}-400 flex items-center justify-center cursor-pointer w-full font-semibold`}>{props.text}</div>
+      <div className={`border-1 h-14 rounded-2xl border-blue-400 px-5 text-blue-400 flex items-center justify-center cursor-pointer w-full font-semibold`}>{props.text}</div>
+    </Link>
+  ) : (
+    <Link
+      href={{
+        pathname: props.mylink,
+      }}
+    >
+      <div className={`border-1 h-14 rounded-2xl border-red-400 px-5 text-red-400 flex items-center justify-center cursor-pointer w-full font-semibold`}>{props.text}</div>
     </Link>
   );
 };
